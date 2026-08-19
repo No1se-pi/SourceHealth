54a89dad7d4b9f8be4a067b9688857c2e07dba8c
No1se-pi <skillday450@gmail.com>
2026-08-20 00:20:53 +0300

    Проба перв

--- test/GitActivity/date.py
+++ test/GitActivity/date.py
@@ -2,6 +2,16 @@ import datetime
 
 def commit_periodicity() -> list:
 
+    def min_range(commits_date):
+
+            min_len = commits_date[0] - commits_date[1]
+
+            for i in range(1, len(commits_date)-1):
+                new_min = commits_date[i] - commits_date[i+1]
+                if new_min < min_len:
+                    min_len = new_min
+            return min_len
+
     log_file='temp/git_log.txt' #Лучше заменить на более стабильный приём пути файла
     commits_date=[]
 
@@ -16,4 +26,12 @@ def commit_periodicity() -> list:
 
                 commits_date.append(date) # Добавляем в конец массива послднюю дату.
 
-commit_periodicity()
+    total_commits = len(commits_date)
+    first_commit = commits_date[-1]
+    last_commit_date = commits_date[0]
+    days_since_last_commit = datetime.datetime.now(datetime.timezone.utc) - commits_date[0]
+    min_commit_gap = min_range(commits_date)
+
+    return(min_commit_gap)
+
+print(commit_periodicity())
--- test/GitActivity/save_log.py
+++ test/GitActivity/save_log.py
@@ -1,8 +1,15 @@
 import subprocess
 
-def save_log() -> None:
+def save_log(path: str) -> None:
+
+    subprocess.run(
+        ["git", "log"],
+        cwd=path,
+        stdout=f
+    )
+
     # Открываем файл для записи ('w' - перезапись, 'a' - добавление в конец)
     with open("temp/git_log.txt", "w", encoding="utf-8") as f:
         subprocess.run(["git", "log"], stdout=f)
 
-save_log()
+save_log("D:\\Программирование\\VPNyasha")
